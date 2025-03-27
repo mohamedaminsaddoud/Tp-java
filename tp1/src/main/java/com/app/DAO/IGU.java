2ac6fbe0fcac448fb0794fd06d4df7cb35456a1d
mohamedaminsaddoud <164727714+mohamedaminsaddoud@users.noreply.github.com>
2025-03-27 13:26:24 +0100

    Delete tp1 directory

--- tp1/src/main/java/com/app/DAO/IGU.java
+++
@@ -1,8 +0,0 @@
-package com.app.DAO;
-
-import com.app.metier.User;
-
-public interface IGU {
-    abstract public void saveUser(String email, String password);
-    abstract public User validateUser(String email, String password);
-}
--- tp1/src/main/java/com/app/metier/IGUM.java
+++
@@ -1,7 +0,0 @@
-package com.app.metier;
-
-public interface IGUM {
-    abstract public void saveUser(String email, String password);
-    abstract public User validateUser(String email, String password);
-
-}
